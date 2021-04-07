1b6ae07e7bf1f4ad1a3432169b3d5bbddb795a90
AliaksandrYenza <sashayenza1996@gmail.com>
2021-04-07 19:00:02 -0400

    just example

---
+++ AY/str_tasks/reverse_str.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/reverse-string/
+#
+# Write a function that reverses a string. The input string is given as an array of characters s.
+# Example 1:
+# Input: s = ["h","e","l","l","o"]
+# Output: ["o","l","l","e","h"]
+
+
+class Solution:
+    def reverseString(self, s: list[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s.reverse()
+        print(s)
+
+if __name__ == '__main__':
+    ls = ["h","e","l","l","o"]
+    Solution.reverseString(0,ls)
