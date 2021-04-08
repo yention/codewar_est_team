45e1865e0a0d78f0db1506c6572a364259e3548a
AliaksandrYenza <sashayenza1996@gmail.com>
2021-04-08 10:23:54 -0400

    1st example

---
+++ AY/0_my_questions/tast_tast.py
@@ -0,0 +1,1 @@
+# file with task which i cannot solve
--- AY/str_tasks/reverse_str.py
+++ AY/str_tasks/reverse_str.py
@@ -1,9 +1,11 @@
 # https://leetcode.com/problems/reverse-string/
-#
-# Write a function that reverses a string. The input string is given as an array of characters s.
-# Example 1:
-# Input: s = ["h","e","l","l","o"]
-# Output: ["o","l","l","e","h"]
+
+'''
+    Write a function that reverses a string. The input string is given as an array of characters s.
+    Example 1:
+    Input: s = ["h","e","l","l","o"]
+    Output: ["o","l","l","e","h"]
+'''
 
 
 class Solution:
